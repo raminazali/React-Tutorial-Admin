3c467b496d5136095da9cff3c5d2794d06c540ea
ramin azali <raminazali8@gmail.com>
2023-09-05 10:03:34 +0330

    adding Navbar , Menu , Footer And Some Other Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,58 @@
-function App() {
+import { RouterProvider, Outlet, createBrowserRouter } from "react-router-dom";
+import Home from "./Pages/home/Home";
+import Products from "./Pages/products/Products";
+import Users from "./Pages/users/Users";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import Login from "./Pages/login/Login";
+import "./styles/global.scss";
 
-  return (
-    <div>Hello World</div>
-  )
+function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "users",
+          element: <Users />,
+        },
+        {
+          path: "products",
+          element: <Products />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
