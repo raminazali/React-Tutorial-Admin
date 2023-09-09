1d183000ccc89862729dec4389618cf41e6bb922
ramin azali <raminazali8@gmail.com>
2023-09-09 22:28:56 +0330

    Some Chrts And Menu Added

--- src/Pages/home/Home.tsx
+++ src/Pages/home/Home.tsx
@@ -1,7 +1,37 @@
+import ChartBox from "../../components/chartBox/chartBox";
+import TopBox from "../../components/topBox/TopBox";
+import {
+  chartBoxConversion,
+  chartBoxProduct,
+  chartBoxRevenue,
+  chartBoxUser,
+} from "../../data";
 import "./home.scss";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <div className="box box1">
+        <TopBox />
+      </div>
+      <div className="box box2">
+        <ChartBox {...chartBoxUser} />
+      </div>
+      <div className="box box3">
+        <ChartBox {...chartBoxProduct} />
+      </div>
+      <div className="box box4">box4</div>
+      <div className="box box5">
+        <ChartBox {...chartBoxRevenue} />
+      </div>
+      <div className="box box6">
+        <ChartBox {...chartBoxConversion} />
+      </div>
+      <div className="box box7">box7</div>
+      <div className="box box8">box8</div>
+      <div className="box box9">box9</div>
+    </div>
+  );
 };
 
 export default Home;
